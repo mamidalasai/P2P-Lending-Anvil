3e947095aa28986df48c469434142e3ea4e49101
saimamidala333@gmail.com <saimamidala333@gmail.com>
2024-02-05 10:59:38 +0000

    Edited admin_server

--- server_code/admin_server.py
+++ server_code/admin_server.py
@@ -92,15 +92,17 @@ def add_data(customer_id, email, password, name, number):
   tables.app_tables.fin_user_profile.add_row(customer_id=customer_id, email_user=email, full_name=name, mobile=number)
 
 @anvil.server.callable
+def lender(name, gender, date_of_birth):
+  tables.app_tables.fin_user_profile.add_row(full_name=name, gender=gender,date_of_birth=date_of_birth)
+  
+@anvil.server.callable
 def login_data():
   data = tables.app_tables.users.search()
-  
   return data
 
-def lender_data(name, gender, date_of_birth):
-  tables.app_tables.fin_user_profile.add_row(full_name=name, gender=gender, date_of_birth=date_of_birth)
-  profile = tables.app_tables.fin_user_profile.search()
-  return profile
+def profile():
+  data = tables.app_tables.fin_user_profile.search()
+  return data
 
 @anvil.server.callable
 def get_extension_data():
