f5f27d985e69390811baef44908a6293f3163d68
saimamidala333@gmail.com <saimamidala333@gmail.com>
2024-02-06 06:41:33 +0000

    Edited mobile_server

--- server_code/mobile_server.py
+++ server_code/mobile_server.py
@@ -8,3 +8,32 @@ from anvil.tables import app_tables
 import anvil.server
 
 
+@anvil.server.callable()
+def get_table_data():
+    # Fetch and return data from Anvil table
+    # For example, fetch data from Anvil table 'my_data'
+    data = tables.app_tables.fin_loan_details.search()
+    return data
+
+@anvil.server.callable
+def add_data(customer_id, email, password, name, number):
+  tables.app_tables.users.add_row(email=email, password_hash=password)
+  tables.app_tables.fin_user_profile.add_row(customer_id=customer_id, email_user=email, full_name=name, mobile=number)
+
+@anvil.server.callable
+def lender(name, gender, date_of_birth):
+  tables.app_tables.fin_user_profile.add_row(full_name=name, gender=gender,date_of_birth=date_of_birth)
+  
+@anvil.server.callable
+def login_data():
+  data = tables.app_tables.users.search()
+  return data
+@anvil.server.callable
+def profile():
+  data = tables.app_tables.fin_user_profile.search()
+  return data
+
+@anvil.server.callable
+def get_extension_data():
+    data = tables.app_tables.fin_extends_loan.search()
+    return data
